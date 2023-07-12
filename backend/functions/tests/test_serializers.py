c8db0245c62376ed2cb27a0a50dbe36ef25c7bdb
abdoafage <abdo.afage2@gmail.com>
2023-07-12 03:20:57 +0300

    add test_serializer

---
+++ backend/functions/tests/test_serializers.py
@@ -0,0 +1,25 @@
+import pytest
+from functions.models import Function
+from functions.serializers import FunctionSerializer
+
+
+@pytest.mark.django_db
+def test_function_serializer(AdminUser):
+    function = FunctionSerializer(
+        data={
+            "name": "find_min",
+            "body": "def find_min(nums):\n    return min(nums)",
+            "numberOfCalls": 0,
+        }
+    )
+    function.is_valid(raise_exception=True)
+    function.save(owner=AdminUser)
+
+    assert Function.objects.count() == 1
+
+    func = Function.objects.all()[0]
+
+    assert func.name == "find_min"
+    assert func.body == "def find_min(nums):\n    return min(nums)"
+    assert func.numberOfCalls == 0
+    assert func.owner == AdminUser
